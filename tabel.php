85fbb49e40987235c3a232fff77cf8dae38bcdbe
Ormet99 <ormet99@gmail.com>
2018-05-14 12:20:49 +0300

    Rea lisamise funktsioon ja väljastamisfunktsioon

--- tabel.php
+++ tabel.php
@@ -9,4 +9,27 @@ class tabel{
         $this->pealkirjad = $pealkirjad;
         $this->veergudearv = count($pealkirjad);
     }
+
+    function lisaRida($rida){
+        if(count($rida) != $this->veergudearv){
+            return false;
+        }
+        array_push($this->tabelisisu, $rida);
+        return true;
+    }
+
+    function prinditabel(){
+        echo '<pre>';
+        foreach ($this->pealkirjad as $pealkiri){
+            echo '<b>'.$pealkiri.'</b>'.' ';
+        }
+        echo "\n";
+        foreach ($this->tabelisisu as $rida){
+            foreach ($rida as $reaElement){
+                echo $reaElement.' ';
+            }
+            echo "\n";
+        }
+        echo '</pre>';
+    }
 }
