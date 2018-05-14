137a3af7dffbf0926d7b08704a489a463dff89fd
Ormet99 <ormet99@gmail.com>
2018-05-14 12:14:29 +0300

    Konstruktori lisamine

--- tabel.php
+++ tabel.php
@@ -4,4 +4,9 @@ class tabel{
     var $tabelisisu = array();
     var $pealkirjad = array();
     var $veergudarv;
+
+    public function __construct(array $pealkirjad){
+        $this->pealkirjad = $pealkirjad;
+        $this->veergudearv = count($pealkirjad);
+    }
 }
